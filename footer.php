f0afa425dc97cf92f5562ce7cc6949aab44c2b1c
Eduardo Collado <edu@eduangi.com>
2026-02-25 12:38:40 +0100

    feat: añadir enlace de verificación Mastodon oculto en footer

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php declare(strict_types=1); ?>
 <footer class="site-footer">
   <a href="https://github.com/educollado/EasyPodcast" target="_blank" rel="noopener noreferrer">EasyPodcast</a>, hecho en <strong>Europa</strong> con ❤️ por <a href="https://www.eduardocollado.com" target="_blank" rel="noopener noreferrer">Eduardo Collado</a>
+  <span style="font-size:0;"><a rel="me" href="https://mastodon.social/@ecollado">Mastodon</a></span>
 </footer>
